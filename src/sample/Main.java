d59818166916e0250022c8dc9b3874a8d719d53a
Kenneth Culala <kaculala@gmail.com>
2017-11-16 12:18:23 -0800

    add toolbar for transformations

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,15 +2,13 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Orientation;
 import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -28,8 +26,9 @@ import java.util.List;
 
 public class Main extends Application {
 
-    private static final int WIDTH = 500;
-    private static final int HEIGHT = 500;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 550;
+    private static final int GRID_SPACING = 10;
 
     private List<Point3D> vertices = new ArrayList<>();
     private List<Pair> lines = new ArrayList<>();
@@ -42,13 +41,56 @@ public class Main extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, WIDTH, HEIGHT, Color.BLACK);
 
-        Pane wrapper = new Pane();
-        Canvas canvas = new Canvas(scene.getWidth(), scene.getHeight());
-        wrapper.getChildren().add(canvas);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Usage");
+        alert.setHeaderText(null);
+        alert.setContentText("Please select two (2) files:\n" +
+                "The first file should contain a list of 3 numbers representing a vertex\n" +
+                "e.g. 10 10 0\n" +
+                "The second file should contain a list of 2 numbers representing a line\n" +
+                "e.g. 1 2");
+        alert.showAndWait();
 
+        // toolbar
+        Button translateLeft = new Button("Left");
+        Button translateRight = new Button("Right");
+        Button translateUp = new Button("Up");
+        Button translateDown = new Button("Down");
+        Button scaleUp = new Button("Scale Up");
+        Button scaleDown = new Button("Scale Down");
+        Button rotateX = new Button("Rotate about X");
+        Button rotateY = new Button("Rotate about Y");
+        Button rotateZ = new Button("Rotate Down");
+        Button shearLeft = new Button("Shear Left");
+        Button shearRight = new Button("Shear Right");
+        Button restore = new Button("Restore");
+        Button exit = new Button("Exit");
+        Button spinX = new Button("Spin X");
+        Button spinY = new Button("Spin Y");
+        Button spinZ = new Button("Spin Z");
+
+        ToolBar toolBar = new ToolBar();
+        toolBar.setOrientation(Orientation.VERTICAL);
+        toolBar.getItems().addAll(translateLeft, translateRight, translateUp, translateDown, new Separator(),
+                                  scaleUp, scaleDown, new Separator(),
+                                  rotateX, rotateY, rotateZ, new Separator(),
+                                  shearLeft, shearRight, new Separator(),
+                                  restore, exit, new Separator(),
+                                  spinX, spinY, spinZ);
+
+        // canvas
+        Pane wrapper = new Pane();
+        Canvas canvas = new Canvas();
+        wrapper.getChildren().add(canvas);
+        canvas.widthProperty().bind(wrapper.widthProperty());
+        canvas.heightProperty().bind(wrapper.heightProperty());
+
+        // menu
         MenuBar menuBar = new MenuBar();
         Menu menu = new Menu("File");
         MenuItem newData = new MenuItem("New data...");
+        menu.getItems().add(newData);
+        menuBar.getMenus().add(menu);
 
         newData.addEventHandler(ActionEvent.ACTION,
                 event -> {
@@ -72,26 +114,19 @@ public class Main extends Application {
                     }
                 }
         );
-        menu.getItems().add(newData);
-        menuBar.getMenus().add(menu);
 
+        // stage
         BorderPane borderPane = new BorderPane();
+        borderPane.prefHeightProperty().bind(scene.heightProperty());
+        borderPane.prefWidthProperty().bind(scene.widthProperty());
+
         borderPane.setTop(menuBar);
         borderPane.setCenter(wrapper);
+        borderPane.setRight(toolBar);
 
         root.getChildren().add(borderPane);
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Usage");
-        alert.setHeaderText(null);
-        alert.setContentText("Please select two (2) files:\n" +
-                "The first file should contain a list of 3 numbers representing a vertex\n" +
-                "e.g. 10 10 0\n" +
-                "The second file should contain a list of 2 numbers representing a line\n" +
-                "e.g. 1 2");
-        alert.showAndWait();
     }
 
     private void draw(GraphicsContext gc) {
@@ -135,11 +170,11 @@ public class Main extends Application {
         String[] tokens = line.split(" ");
         if(tokens.length != 3)
             return null;
-        double xCoordinate = Double.parseDouble(tokens[0]) * 10;
-        double yCoordinate = Double.parseDouble(tokens[1]) * 10;
+        double xCoordinate = Double.parseDouble(tokens[0]) * GRID_SPACING;
+        double yCoordinate = Double.parseDouble(tokens[1]) * GRID_SPACING;
         if (yCoordinate > maxY)
             maxY = yCoordinate;
-        double zCoordinate = Double.parseDouble(tokens[2]) * 10;
+        double zCoordinate = Double.parseDouble(tokens[2]) * GRID_SPACING;
         return new Point3D(xCoordinate, yCoordinate, zCoordinate);
     }
 
