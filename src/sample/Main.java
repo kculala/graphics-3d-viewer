1d7a1a7cd264a8092789791a4f8679588e30766a
Kenneth Culala <kaculala@gmail.com>
2017-11-16 21:50:40 -0800

    add graphics for toolbar buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -16,10 +18,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
@@ -30,6 +29,68 @@ public class Main extends Application {
     private static final int HEIGHT = 550;
     private static final int GRID_SPACING = 10;
 
+    private static final String SCENE_TITLE = "COMP 4560 Assignment 5 - A00797801";
+    private static final String USAGE_TITLE = "Usage";
+    private static final String INSTRUCTIONS = "Please select two (2) files:\n" +
+            "The first file should contain a list of 3 numbers representing a vertex\n" +
+            "e.g. 10 10 0\n" +
+            "The second file should contain a list of 2 numbers representing a line\n" +
+            "e.g. 1 2";
+    private static final String MENU_LABEL_FILE = "File";
+    private static final String MENU_ITEM_LABEL_NEW_DATA = "New DATA...";
+
+    private enum ImagePaths {
+        TRANSLATE_LEFT ("left"),
+        TRANSLATE_RIGHT ("right"),
+        TRANSLATE_UP ("up"),
+        TRANSLATE_DOWN ("down"),
+        SCALE_UP ("scale-up"),
+        SCALE_DOWN ("scale-down"),
+        ROTATE_X ("rotate-x"),
+        ROTATE_Y ("rotate-y"),
+        ROTATE_Z ("rotate-z"),
+        SHEAR_LEFT ("shear-left"),
+        SHEAR_RIGHT ("shear-right"),
+        RESTORE ("home"),
+        EXIT ("exit");
+
+        private final String path;
+
+        ImagePaths(final String s) {
+            path = "resources/images/" + s + ".png";
+        }
+
+        public String toString() {
+            return this.path;
+        }
+    }
+
+    private enum Tooltips {
+        TRANSLATE_LEFT ("Translate Left 25"),
+        TRANSLATE_RIGHT ("Translate Right 25"),
+        TRANSLATE_UP ("Translate Up 10"),
+        TRANSLATE_DOWN ("Translate Down 10"),
+        SCALE_UP ("Scale 10% Up"),
+        SCALE_DOWN ("Scale 10% Down"),
+        ROTATE_X ("Rotate About X"),
+        ROTATE_Y ("Rotate About Y"),
+        ROTATE_Z ("Rotate About Z"),
+        SHEAR_LEFT ("Shear Left in Y"),
+        SHEAR_RIGHT ("Shear Right in Y"),
+        RESTORE ("Restore Initial Image"),
+        EXIT ("Exit");
+
+        private final String text;
+
+        Tooltips(final String s) {
+            text = s;
+        }
+
+        public String toString() {
+            return this.text;
+        }
+    }
+
     private List<Point3D> vertices = new ArrayList<>();
     private List<Pair> lines = new ArrayList<>();
     private double maxY = 0;
@@ -37,45 +98,106 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        primaryStage.setTitle("COMP 4560 Assignment 5 - A00797801");
+        primaryStage.setTitle(SCENE_TITLE);
         Group root = new Group();
         Scene scene = new Scene(root, WIDTH, HEIGHT, Color.BLACK);
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Usage");
+        alert.setTitle(USAGE_TITLE);
         alert.setHeaderText(null);
-        alert.setContentText("Please select two (2) files:\n" +
-                "The first file should contain a list of 3 numbers representing a vertex\n" +
-                "e.g. 10 10 0\n" +
-                "The second file should contain a list of 2 numbers representing a line\n" +
-                "e.g. 1 2");
-        alert.showAndWait();
+        alert.setContentText(INSTRUCTIONS);
+//        alert.showAndWait();
 
         // toolbar
-        Button translateLeft = new Button("Left");
-        Button translateRight = new Button("Right");
-        Button translateUp = new Button("Up");
-        Button translateDown = new Button("Down");
-        Button scaleUp = new Button("Scale Up");
-        Button scaleDown = new Button("Scale Down");
-        Button rotateX = new Button("Rotate about X");
-        Button rotateY = new Button("Rotate about Y");
-        Button rotateZ = new Button("Rotate Down");
-        Button shearLeft = new Button("Shear Left");
-        Button shearRight = new Button("Shear Right");
-        Button restore = new Button("Restore");
-        Button exit = new Button("Exit");
-        Button spinX = new Button("Spin X");
-        Button spinY = new Button("Spin Y");
-        Button spinZ = new Button("Spin Z");
+        Image imageLeft = new Image(new FileInputStream(ImagePaths.TRANSLATE_LEFT.toString()));
+        Button buttonTranslateLeft = new Button();
+        buttonTranslateLeft.setGraphic(new ImageView(imageLeft));
+        Tooltip tooltipLeft = new Tooltip(Tooltips.TRANSLATE_LEFT.toString());
+        buttonTranslateLeft.setTooltip(tooltipLeft);
+
+        Image imageRight = new Image(new FileInputStream(ImagePaths.TRANSLATE_RIGHT.toString()));
+        Button buttonTranslateRight = new Button();
+        buttonTranslateRight.setGraphic(new ImageView(imageRight));
+        Tooltip tooltipRight = new Tooltip(Tooltips.TRANSLATE_RIGHT.toString());
+        buttonTranslateRight.setTooltip(tooltipRight);
+
+        Image imageUp = new Image(new FileInputStream(ImagePaths.TRANSLATE_UP.toString()));
+        Button buttonTranslateUp = new Button();
+        buttonTranslateUp.setGraphic(new ImageView(imageUp));
+        Tooltip tooltipUp = new Tooltip(Tooltips.TRANSLATE_UP.toString());
+        buttonTranslateUp.setTooltip(tooltipUp);
+
+        Image imageDown = new Image(new FileInputStream(ImagePaths.TRANSLATE_DOWN.toString()));
+        Button buttonTranslateDown = new Button();
+        buttonTranslateDown.setGraphic(new ImageView(imageDown));
+        Tooltip tooltipDown = new Tooltip(Tooltips.TRANSLATE_DOWN.toString());
+        buttonTranslateDown.setTooltip(tooltipDown);
+
+        Image imageScaleUp = new Image(new FileInputStream(ImagePaths.SCALE_UP.toString()));
+        Button buttonScaleUp = new Button();
+        buttonScaleUp.setGraphic(new ImageView(imageScaleUp));
+        Tooltip tooltipScaleUp = new Tooltip(Tooltips.SCALE_UP.toString());
+        buttonScaleUp.setTooltip(tooltipScaleUp);
+
+        Image imageScaleDown = new Image(new FileInputStream(ImagePaths.SCALE_DOWN.toString()));
+        Button buttonScaleDown = new Button();
+        buttonScaleDown.setGraphic(new ImageView(imageScaleDown));
+        Tooltip tooltipScaleDown = new Tooltip(Tooltips.SCALE_DOWN.toString());
+        buttonScaleDown.setTooltip(tooltipScaleDown);
+
+        Image imageRotateX = new Image(new FileInputStream(ImagePaths.ROTATE_X.toString()));
+        Button buttonRotateX = new Button();
+        buttonRotateX.setGraphic(new ImageView(imageRotateX));
+        Tooltip tooltipRotateX = new Tooltip(Tooltips.ROTATE_X.toString());
+        buttonRotateX.setTooltip(tooltipRotateX);
+
+        Image imageRotateY = new Image(new FileInputStream(ImagePaths.ROTATE_Y.toString()));
+        Button buttonRotateY = new Button();
+        buttonRotateY.setGraphic(new ImageView(imageRotateY));
+        Tooltip tooltipRotateY = new Tooltip(Tooltips.ROTATE_Y.toString());
+        buttonRotateY.setTooltip(tooltipRotateY);
+
+        Image imageRotateZ = new Image(new FileInputStream(ImagePaths.ROTATE_Z.toString()));
+        Button buttonRotateZ = new Button();
+        buttonRotateZ.setGraphic(new ImageView(imageRotateZ));
+        Tooltip tooltipRotateZ = new Tooltip(Tooltips.ROTATE_Z.toString());
+        buttonRotateZ.setTooltip(tooltipRotateZ);
+
+        Image imageShearLeft = new Image(new FileInputStream(ImagePaths.SHEAR_LEFT.toString()));
+        Button buttonShearLeft = new Button();
+        buttonShearLeft.setGraphic(new ImageView(imageShearLeft));
+        Tooltip tooltipShearLeft = new Tooltip(Tooltips.SHEAR_LEFT.toString());
+        buttonShearLeft.setTooltip(tooltipShearLeft);
+
+        Image imageShearRight = new Image(new FileInputStream(ImagePaths.SHEAR_RIGHT.toString()));
+        Button buttonShearRight = new Button();
+        buttonShearRight.setGraphic(new ImageView(imageShearRight));
+        Tooltip tooltipShearRight = new Tooltip(Tooltips.SHEAR_RIGHT.toString());
+        buttonShearRight.setTooltip(tooltipShearRight);
+
+        Image imageRestore = new Image(new FileInputStream(ImagePaths.RESTORE.toString()));
+        Button buttonRestore = new Button();
+        buttonRestore.setGraphic(new ImageView(imageRestore));
+        Tooltip tooltipRestore = new Tooltip(Tooltips.RESTORE.toString());
+        buttonRestore.setTooltip(tooltipRestore);
+
+        Image imageExit = new Image(new FileInputStream(ImagePaths.EXIT.toString()));
+        Button buttonExit = new Button();
+        buttonExit.setGraphic(new ImageView(imageExit));
+        Tooltip tooltipExit = new Tooltip(Tooltips.EXIT.toString());
+        buttonExit.setTooltip(tooltipExit);
+
+        Button spinX = new Button("X");
+        Button spinY = new Button("Y");
+        Button spinZ = new Button("Z");
 
         ToolBar toolBar = new ToolBar();
         toolBar.setOrientation(Orientation.VERTICAL);
-        toolBar.getItems().addAll(translateLeft, translateRight, translateUp, translateDown, new Separator(),
-                                  scaleUp, scaleDown, new Separator(),
-                                  rotateX, rotateY, rotateZ, new Separator(),
-                                  shearLeft, shearRight, new Separator(),
-                                  restore, exit, new Separator(),
+        toolBar.getItems().addAll(buttonTranslateLeft, buttonTranslateRight, buttonTranslateUp, buttonTranslateDown, new Separator(),
+                                  buttonScaleUp, buttonScaleDown, new Separator(),
+                                  buttonRotateX, buttonRotateY, buttonRotateZ, new Separator(),
+                                  buttonShearLeft, buttonShearRight, new Separator(),
+                                  buttonRestore, buttonExit, new Separator(),
                                   spinX, spinY, spinZ);
 
         // canvas
@@ -87,8 +209,8 @@ public class Main extends Application {
 
         // menu
         MenuBar menuBar = new MenuBar();
-        Menu menu = new Menu("File");
-        MenuItem newData = new MenuItem("New data...");
+        Menu menu = new Menu(MENU_LABEL_FILE);
+        MenuItem newData = new MenuItem(MENU_ITEM_LABEL_NEW_DATA);
         menu.getItems().add(newData);
         menuBar.getMenus().add(menu);
 
